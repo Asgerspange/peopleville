86b60cd677bba0077baf5f796410d4b31848ee42
test <test>
2024-10-23 13:06:56 +0200

    work

--- PeopleVilleEngine/Villagers/BaseVillager.cs
+++ PeopleVilleEngine/Villagers/BaseVillager.cs
@@ -22,6 +22,11 @@ public abstract class BaseVillager
         PersonalWallet = new Wallet("$", 100m); // Initialiserer pungen med $ og 100 dollars som start kapital
     }
 
+    public string PerformHobby()
+    {
+        return $"{FirstName} {LastName} is {Hobby}.";
+    }
+
     public override string ToString()
     {
         return $"{FirstName} {LastName} {Hobby} ({Age} years)";
