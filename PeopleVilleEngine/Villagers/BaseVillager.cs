25af5c5b3f3bf1e9a72281cd45e03928bb6bf5a9
test <test>
2024-10-23 11:16:06 +0200

    feat hobby

--- PeopleVilleEngine/Villagers/BaseVillager.cs
+++ PeopleVilleEngine/Villagers/BaseVillager.cs
@@ -6,6 +6,7 @@ public abstract class BaseVillager
     public int Age { get; protected set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string Hobby { get; set; }
     public bool IsMale { get; set; }
     private Village _village;
     public ILocation? Home { get; set; } = null;
@@ -16,10 +17,11 @@ public abstract class BaseVillager
         _village = village;
         IsMale = RNG.GetInstance().Next(0, 2) == 0;
         (FirstName, LastName) = village.VillagerNameLibrary.GetRandomNames(IsMale);
+        (Hobby) = village.VillagerHobbyLibrary.GetRandomHobby();
     }
 
     public override string ToString()
     {
-        return $"{FirstName} {LastName} ({Age} years)";
+        return $"{FirstName} {LastName} {Hobby} ({Age} years)";
     }
 }
