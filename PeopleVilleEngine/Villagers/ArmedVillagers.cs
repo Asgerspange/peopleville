d75d666db02629c717d37717a325066dc421c456
Jeppe Hein <jepp725f@edu.sde.dk>
2024-10-23 10:19:58 +0200

    Nedarvningsklasse "ArmedVillager"
    Nedarber fra "BaseVillager"
    Præsentere en string af våben og assigner til en basevillager

---
+++ PeopleVilleEngine/Villagers/ArmedVillagers.cs
@@ -0,0 +1,28 @@
+﻿using PeopleVilleEngine;
+using PeopleVilleEngine.Locations;
+using System.Collections.Generic;
+
+public class ArmedVillager : BaseVillager
+{
+    public string Weapon { get; private set; }
+    private static readonly List<string> Weapons = new List<string> { "Uzi", "Bat", "Hammer", "Kniv", "Morgenstjerne", "Knojern", "Pisk" };
+
+    //constructor kalder base klassen "Villager" "Weapon" initialisereres ved at kalde metoden "AssignRandomWeapon"
+    public ArmedVillager(Village village) : base(village)
+    {
+        Weapon = AssignRandomWeapon();
+    }
+
+    private string AssignRandomWeapon()
+    {
+        var rng = RNG.GetInstance();
+        int index = rng.Next(Weapons.Count);
+        return Weapons[index];
+    }
+
+    //returnerer en string til at beskrive villager samt våben.
+    public override string ToString()
+    {
+        return $"{base.ToString()} - Armed with: {Weapon}";
+    }
+}
