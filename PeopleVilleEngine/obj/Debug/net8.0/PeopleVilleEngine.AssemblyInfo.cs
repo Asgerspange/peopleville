06b2b4c0cc20b46cfb4b83f8d7d6bf18dc34e6b6
Jeppe Hein <jepp725f@edu.sde.dk>
2024-10-25 10:35:51 +0200

    Json filer rigtig mappe + retssystem

--- PeopleVilleEngine/obj/Debug/net8.0/PeopleVilleEngine.AssemblyInfo.cs
+++ PeopleVilleEngine/obj/Debug/net8.0/PeopleVilleEngine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeopleVilleEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c1eac1f4831ba82643496287b77d753cf629dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07a88be92031040fcde13d87e94dd4d69fc4d10")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeopleVilleEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeopleVilleEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
