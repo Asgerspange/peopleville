5f2318c082de09e68f5062b8c9d602d33c14827f
asger <asger>
2024-10-25 10:29:53 +0200

    reset

--- PeopleVilleEngine/Time.cs
+++ PeopleVilleEngine/Time.cs
@@ -7,29 +7,17 @@ namespace PeopleVilleEngine
     {
         public string Day { get; set; } = "Day 1";
         public int dayCount = 1;
-        private readonly Village _village;
 
         public event Action NewDayStarted;
 
-
-
-        public Time(Village village)
-        {
-            _village = village;
-        }
 
         public string UpdateDay()
         {
             dayCount++;
             Day = $"Day {dayCount}";
             OnNewDayStarted();
-            TriggerDailyEvents();
 
             return this.ToString();
-        }
-        private void TriggerDailyEvents()
-        {
-            
         }
 
         public virtual void OnNewDayStarted()
